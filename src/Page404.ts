b6da58cfd766db15a8e42d30ea84cfeae0a50f7f
Filip Kania <fkania2@gmail.com>
2022-01-21 12:18:24 +0100

    refactor: change message on 404

--- src/Page404.ts
+++ src/Page404.ts
@@ -2,6 +2,7 @@ import { returnJSON } from "./utils/returnJSON";
 
 export default (): Response => {
 	return returnJSON({
-		"error": 404
+		"error": 404,
+		"message": "Shorten your URL under /api/shorten!"
 	});
 }
